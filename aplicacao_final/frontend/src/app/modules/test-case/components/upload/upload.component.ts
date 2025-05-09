82e4fff98bb9ed3e2a6e6a7f23a24840918e8f70
Leonardo Ribeiro <leonardo25palmeira250@gmail.com>
2025-05-09 17:40:05 -0300

    Inicio do front consumindo o back

---
+++ aplicacao_final/frontend/src/app/modules/test-case/components/upload/upload.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { TestCaseService } from '../../services/test-case.service';
+import { UploadResponse } from '../../models/test-case.models';
+
+@Component({
+  selector: 'app-test-case-upload',
+  templateUrl: './upload.component.html'
+})
+export class TestCaseUploadComponent {
+  userId = '';
+  description = '';
+  yamlFiles: File[] = [];
+  jsonFiles: File[] = [];
+  uploadResponse?: UploadResponse;
+
+  constructor(private testCaseService: TestCaseService) {}
+
+  onYamlFilesSelected(event: any): void {
+    this.yamlFiles = Array.from(event.target.files);
+  }
+
+  onJsonFilesSelected(event: any): void {
+    this.jsonFiles = Array.from(event.target.files);
+  }
+
+  uploadFiles(): void {
+    this.testCaseService
+      .uploadTestCasesBatch(this.userId, this.description, this.yamlFiles, this.jsonFiles)
+      .subscribe({
+        next: (response) => {
+          this.uploadResponse = response;
+        },
+        error: (err) => {
+          this.uploadResponse = {
+            success: false,
+            message: err.error.message || 'Upload failed'
+          };
+        }
+      });
+  }
+}
