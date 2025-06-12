c7fc25639fc94de2ce2ac3b46be16fa9ea8a5a13
Leonardo Ribeiro <leonardo25palmeira250@gmail.com>
2025-06-12 06:18:57 -0300

    Ajuste na leitura yaml

--- aplicacao/backend/Models/Input/YamlTestCaseDefinition.cs
+++ aplicacao/backend/Models/Input/YamlTestCaseDefinition.cs
@@ -9,34 +9,34 @@ namespace FHIRUT.API.Models.Input
 
         public string Description { get; set; }
 
-        public YamlContext Context { get; set; }
+        public YamlContext Context { get; set; } = new YamlContext();
 
         [YamlMember(Alias = "instance_path")]
-        public List<string> InstancePath { get; set; }
+        public List<string> InstancePath { get; set; } = new List<string>();
 
         [YamlMember(Alias = "expected_results")]
-        public YamlExpectedResults ExpectedResults { get; set; }
+        public YamlExpectedResults ExpectedResults { get; set; } = new YamlExpectedResults();
     }
 
     public class YamlContext
     {
-        public List<string> Igs { get; set; }
-        public List<string> Profiles { get; set; }
-        public List<string> Resources { get; set; }
+        public List<string> Igs { get; set; } = new List<string>();
+        public List<string> Profiles { get; set; } = new List<string>();
+        public List<string> Resources { get; set; } = new List<string>();
     }
 
     public class YamlExpectedResults
     {
-        public string Status { get; set; }
-        public List<string> Errors { get; set; }
-        public List<string> Warnings { get; set; }
-        public List<string> Informations { get; set; }
-        public YamlInvariant Invariants { get; set; }
+        public string Status { get; set; } = "success"; // Valor padrão
+        public List<string> Errors { get; set; } = new List<string>();
+        public List<string> Warnings { get; set; } = new List<string>();
+        public List<string> Informations { get; set; } = new List<string>();
+        public YamlInvariant Invariants { get; set; } = new YamlInvariant();
     }
 
     public class YamlInvariant
     {
-        public string? Expression { get; set; }
+        public string Expression { get; set; }
         public bool Expected { get; set; } = true;
     }
 }
